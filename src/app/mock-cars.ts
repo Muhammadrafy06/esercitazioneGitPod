8d6b067f3d59919cba8487670f4f34ddf7a6c15d
Muhammadrafy06 <muhammad.abdul@iisgalvanimi.edu.it>
2023-02-16 11:10:30 +0000

    ferrari aggiunta

--- src/app/mock-cars.ts
+++ src/app/mock-cars.ts
@@ -5,5 +5,7 @@ export const CARS: Car[] = [
   { id: 12, name: 'Bravo', costo:20, consumo:20 }, 
   { id: 13, name: 'Tipo', costo:15, consumo:10 }, 
   { id: 14, name: '124_spider' , costo:100, consumo:90 },
-  { id: 15, name: '500', costo:1000, consumo:33 }
+  { id: 15, name: '500', costo:1000, consumo:33 },
+  { id: 15, name: 'ferrari', costo:1000, consumo:33 }
+
 ];
